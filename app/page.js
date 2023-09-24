fac65986995a594c6e3015fa535c57474a2727e2
TamaraMah <tamaramahmoud21@gmail.com>
2023-09-24 14:01:34 -0600

    color

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Link from 'next/link';
+import Head from 'next/head';
 
-export default class Page () {
-  render() {
-    return (
-      <div>
-        <h1>Page</h1>
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-center mb-10 ">Web Development Week 2 Assignment</h1>
+      <div className="flex justify-center space-x-2">
+       <Link href="/" className="text-purple-400 hover:underline">Home</Link>
+       <Link href="/week2" className="text-purple-400 hover:underline ">Week 2</Link>
       </div>
-    )
-  }
+    </main>
+  )
 }
---
+++ app/week2/week2.js
@@ -0,0 +1,17 @@
+export default function week2() {
+  return (
+    <main>
+      <Head>
+        <title>Web Development Week 2 Assignment</title>
+      </Head>
+
+      <h1 className="text-2xl font-bold mb-4">Web Development Week 2 Assignment</h1>
+      
+      <p>Name: Tamara Mahmoud</p>
+      <p>Course: CPRG 306 A</p>
+      <p>GitHub Repository: <Link href="https://github.com/TamaraMah/cprg306-assignments"></Link></p>
+      
+      {/* Add more content as needed */}
+    </main>
+  )
+}
