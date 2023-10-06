c1897d8b3ea314d2e514580ae0e04416c76aa2b2
TamaraMah <tamaramahmoud21@gmail.com>
2023-10-06 13:29:41 -0600

    not done?!??!?!?!?

--- app/week4/page.js
+++ app/week4/page.js
@@ -1,13 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
 import NewItem from './new-item';
 
 const Page = () => {
   return (
     <main>
     <h1 className='text-4xl font-mono mb-6 font-bold text-center mt-14 text-gray-300'>Item Form</h1>
-
-
       <NewItem />
-  
     </main>
   );
 }
