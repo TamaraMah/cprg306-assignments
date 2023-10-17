ae4a10c33cfad15e09d28610d2b5562c4a7ccbb6
TamaraMah <tamaramahmoud21@gmail.com>
2023-10-17 12:27:49 -0600

    done week 7 kinda good enough it works

--- app/week7/item-list.js
+++ app/week7/item-list.js
@@ -18,7 +18,7 @@ const ItemList = ({ items, sortBy, onItemSelect }) => {
     }, [sortBy, items]);
 
     return (
-        <div className="grid grid-cols-2 p-6 m-6 mt-0">
+        <div className="grid grid-cols-2 p-6 m-6 mt-0 ">
         {sortedItems.map((item, index) => (
             <div key={index} style={{border: '1px solid purple', padding: '10px', margin: '10px'}}
                  onClick={() => onItemSelect(item.name)}>
